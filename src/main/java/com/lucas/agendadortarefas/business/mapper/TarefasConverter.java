6f9394958cce629a326c02627ed474f34ebe3139
LucasCaetanoM-Barreto <caetanolucas48@gmail.com>
2025-08-21 14:31:43 -0300

    primeiro commit

---
+++ src/main/java/com/lucas/agendadortarefas/business/mapper/TarefasConverter.java
@@ -0,0 +1,21 @@
+package com.lucas.agendadortarefas.business.mapper;
+
+import com.lucas.agendadortarefas.business.dto.TarefasDTO;
+import com.lucas.agendadortarefas.infrastructure.entity.TarefasEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface TarefasConverter {
+
+
+    TarefasEntity paraTarefaEntity(TarefasDTO dto);
+
+    TarefasDTO paraTarefaDTO(TarefasEntity entity);
+
+    List<TarefasEntity> paraListaTarefasEntity(List<TarefasDTO> dtos);
+
+    List<TarefasDTO> paraListaTarefasDTO(List<TarefasEntity> entities);
+}
---
+++ src/main/java/com/lucas/agendadortarefas/infrastructure/repository/TarefasRepository.java
@@ -0,0 +1,22 @@
+package com.lucas.agendadortarefas.infrastructure.repository;
+
+import com.lucas.agendadortarefas.infrastructure.entity.TarefasEntity;
+import com.lucas.agendadortarefas.infrastructure.enums.StatusNotificacaoEnum;
+import org.springframework.cglib.core.Local;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface TarefasRepository extends MongoRepository<TarefasEntity, String> {
+
+    List<TarefasEntity> findByDataEventoBetweenAndStatusNotificacaoEnum(LocalDateTime dataInicial,
+                                                                        LocalDateTime dataFinal,
+                                                                        StatusNotificacaoEnum status);
+
+    List<TarefasEntity> findByEmailUsuario(String email);
+
+
+}
